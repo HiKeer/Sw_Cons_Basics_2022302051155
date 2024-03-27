8a8c27a636f3ae0e0fde229f1bfe65d109880a13
HiKeer <1047529443@qq.com>
2024-03-27 11:36:57 +0800

    hw4

---
+++ assignment4/Program1/Program1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace assignment4
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = tail.Next;
+            }
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            for(Node<T> n = head; n!= null; n = n.Next  ) 
+            {
+                action(n.Data);
+            }
+        }
+
+    }
+
+
+    internal class Program1
+    {
+        public static void Main(string[] args) 
+        {
+            Random random = new Random();
+            GenericList<double> list = new GenericList<double>();
+            for(int i = 0; i < 10; i++)
+            {
+                list.Add(random.Next(1,1000)/10.0); //生成0.1~100的随机数
+            }
+
+            list.ForEach(a => Console.WriteLine(a));
+
+            double min = 100;
+            double max = 0;
+            double sum = 0;
+            list.ForEach(a =>
+            {
+                if (a < min) min = a;
+                if (a > max) max = a;
+                sum += a;
+            });
+            Console.WriteLine($"min={min}, max={max}, sum={sum:F}");
+
+        }
+    }
+}
---
+++ assignment4/Program2/Program2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics.CodeAnalysis;
+
+namespace assignment4
+{
+    public class Time
+    {
+
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+        public int Second { get; set; }
+
+        public Time(int hour = 0, int minute = 0, int second = 0)
+        {
+            Hour = hour;
+            Minute = minute;
+            Second = second;
+        }
+
+        public override string ToString()
+        {
+            return Hour + ":" + Minute + ":" + Second;
+        }
+    }
+    class AlarmClock
+    {
+        public event Action<AlarmClock> Tick;
+        public event Action<AlarmClock> Alarm;
+
+        public Time TimeNow { get; set; }
+        public Time AlarmTime { get; set; }
+
+        public AlarmClock(int h, int m, int s) 
+        {
+            Tick += a => Console.WriteLine(a.TimeNow);
+            Alarm += a => Console.WriteLine("Alarming!");
+            TimeNow = new Time();
+            AlarmTime = new Time(h,m,s);
+        }
+
+        public void TickAndAlarm() //走时&响铃
+        {
+            int s = 0;
+            Console.WriteLine("The alarmclock starts to tick!");
+            while(true)
+            {
+                DateTime dateTime = DateTime.Now;
+                TimeNow = new Time(dateTime.Hour, dateTime.Minute, dateTime.Second);
+                Tick(this);
+                if(TimeNow.ToString() == AlarmTime.ToString()) { Alarm(this); }
+                Thread.Sleep(1000); //挂起一秒
+                s++;
+                if(s == 100) { break; }
+            }
+            Console.WriteLine("The alarmclock stops to tick!");
+        }
+    }
+
+    internal class Program2
+    {
+        public static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            AlarmClock alarmClock = new AlarmClock(11,28,45);
+            alarmClock.TickAndAlarm();
+        }
+    }
+}
